b42499b0f7c18fe2b7a3228264fe95ea591c980e
Vighnesh <vighneshvelayudhan@gmail.com>
2017-03-15 10:44:37 +0530

    trial

---
+++ website/js/events.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+
+    var mobile = $(document).outerWidth(true) < 768 ? true : false;
+
+    $("#event_list li").mouseenter(function(event) {
+        $(".event_details h3").html(event.target.id);
+    }).click(function(event) {
+        if (!mobile) {
+            if ($(".box_left .event_details").css("display") != "none") {
+                $(".box_left").animate({
+                    "padding": "0px",
+                    "width": "10%"
+                }, {
+                    duration: 400,
+                    queue: false
+                });
+                $(".box_middle").animate({ "width": "40%", "padding": "0px" }, { duration: 400, queue: false });
+                $(".box_left .text2").css({ "transform": "rotate(-90deg)" }, { duration: 400, queue: false });
+                $(".box_left .event_details").css({ "display": "none" });
+            }
+        } else {
+            //the mobile code
+            $(".box_middle").addClass("visible_side_menu");
+        }
+
+    });
+});
---
+++ website/js/slider.js
@@ -0,0 +1,64 @@
+function showTextEffect() {
+    var index = 0,
+        pos = 0;
+    phraseShow = function() {
+        var phrases = ["LEARN", "TEACH", "CREATE"];
+
+        $("#text_effect").html('');
+        pos = 0;
+        for (pos = 0; pos < phrases[index].length; pos++) {
+            $("<span/>", {
+                "class": "effect_char",
+                "style": "animation-delay:" + pos * 0.2 + "s",
+                "html": phrases[index].charAt(pos)
+            }).appendTo("#text_effect");
+        }
+        index++;
+        index = index % 3;
+    }
+
+    phraseShow();
+    setInterval(phraseShow, 3000);
+
+    //The cool button!!
+    $(".parent").click(function() {
+        $(".nav-icon").toggleClass("active");
+        $(".popup_menu").slideToggle();
+    });
+    $(".parent").mouseenter(function() {
+        $(".nav-icon").addClass("hover");
+    });
+    $(".parent").mouseleave(function() {
+        $(".nav-icon").removeClass("hover");
+    });
+}
+
+
+$(document).ready(function() {
+    showTextEffect();
+    
+    //The fullpage initialisation
+
+    $("#fullpage").fullpage({
+        anchors: ['landing', 'about', 'sigs', 'events', 'team', 'projects','lastpage'],
+        menu:"#menu",
+        afterLoad: function(anchorLink, index) {
+            $(".section:nth-of-type(" + index + ") .fly_up").animate({
+                top: "0px",
+                opacity: "1"
+            }, 1000);
+            $(".section:nth-of-type(" + index + ") .fly_up_fast").animate({
+                top: "0px",
+                opacity: "1"
+            }, 600, function() {
+                $(".section:nth-of-type(" + index + ") .fadein").fadeIn();
+            });
+        },
+        onLeave: function(index,nextIndex){
+            if(index == 6 && nextIndex == 7)
+                $('.icon').addClass('icon_show');
+            else
+                $('.icon').removeClass('icon_show');
+        }
+    });
+});
